e1135968ca74f1f3a901c9ec8b626e4a2e1b9066
Unknownlurkr <47453232+Unknownlurkr@users.noreply.github.com>
2022-07-26 19:05:04 -0400

    added more styling, created a logo with white text, added shopping basket icon.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,51 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+//chose better amazon logo with white text
+//'npm i -s @material-ui/core if have trouble with icons
+//dark logo text and white logo text
 
 function Header() {
   return (
     <div className="header">
+
       <img
         className="header__logo"
         src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngmart.com%2Ffiles%2F8%2FAmazon-PNG-Transparent-Image.png&f=1&nofb=1"
       />
+
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
+
       <div className="header__nav">
+
         <div className="header__option">
           <span className="header__optionLineOne">Hello, Guest!</span>
           <span className="header__optionLineTwo">Sign In</span>
         </div>
-        </div>
+        
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">Orders</span>
         </div>
+
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
+
+        <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              0
+            </span>
+          </div>
       </div>
+    </div>
   );
 }
 
